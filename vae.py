2123f93884033fee6ba4ddcf04845846a90086a8
kevin <shi.kevin.98@gmail.com>
2020-12-05 01:13:30 -0800

    initial VAE

--- vae.py
+++ vae.py
@@ -5,27 +5,46 @@ from torch.nn import functional as F
 from typing import *
 
 """ contains vae classes.
-VanillaVae adapted from https://github.com/AntixK/PyTorch-VAE/ and https://github.com/podgorskiy/VAE
-
 """
 
 class VanillaVAE(nn.Module):
-
+    """adapted from https://github.com/AntixK/PyTorch-VAE/ and https://github.com/podgorskiy/VAE
+    """
 
     def __init__(self,
                  in_channels: int,
                  latent_dim: int,
                  res: int,
-                 layer_count: int) -> None:
+                 layer_count: int,
+                 layer_mult: int) -> None:
+        """inits a vanilla VAE
+
+        Parameters
+        ----------
+        in_channels : int
+            number of channels in image data
+        latent_dim : int
+            dimensionality of latent(z) code
+        res : int
+            length of resolution of (square)image data
+            for now only deals with powers of two...
+        layer_count : int
+            number of hidden layers of half of the network
+        layer_mult : int
+            first hidden layer dimension
+        """
         super(VanillaVAE, self).__init__()
 
         self.latent_dim = latent_dim
 
         modules = []
-        hidden_dims = [res*(2**i) for i in range(layer_count)]
+        hidden_dims = [layer_mult*(2**i) for i in range(layer_count)]
         # print (hidden_dims)
         self.last_hdim = hidden_dims[-1]
         image_channels = in_channels
+
+        # TODO: add resnet likeness 
+        # https://github.com/julianstastny/VAE-ResNet18-PyTorch/blob/master/model.py
 
         # Build Encoder
         for h_dim in hidden_dims:
@@ -34,7 +53,7 @@ class VanillaVAE(nn.Module):
                     nn.Conv2d(in_channels, out_channels=h_dim,
                               kernel_size= 3, stride= 2, padding  = 1),
                     nn.BatchNorm2d(h_dim),
-                    nn.LeakyReLU())
+                    nn.ReLU())
             )
             in_channels = h_dim
 
@@ -50,6 +69,7 @@ class VanillaVAE(nn.Module):
 
         hidden_dims.reverse()
 
+        # transpose convolution
         for i in range(len(hidden_dims) - 1):
             modules.append(
                 nn.Sequential(
@@ -145,13 +165,21 @@ class VanillaVAE(nn.Module):
 
         recons_loss = F.mse_loss(recons, input)
 
-        kld_loss = -0.5 * torch.mean(torch.mean(1 + logvar - mu ** 2 - logvar.exp(), dim = 1), dim = 0)
+        kld_loss = -0.5 * torch.mean(torch.sum(1 + logvar - mu ** 2 - logvar.exp(), dim = 1), dim = 0)
+        # if I change the second mean here to a sum we have a huge recons loss, but i dont understand why
+        # the the formula for kl divergence dictates a sum here...
+        # Mean is equivalent to sum, it's just a scalar difference. 
+        # Normally, using adam, if we don't have a composite loss, this scale doesn't matter, 
+        # so if I change the sum to mean, the program should backpropagate the same. 
+        # I changed the mean to a sum and decreased the kld weight, which fixed my problem. 
+        # Basically when I changed the mean to a sum, I put too much weight on the kl weight and
+        # caused the latent distributions to be too strongly bound to the normal guassian.
 
         return {'reconstruction_loss': recons_loss, 'KLD':kld_weight * kld_loss}
 
     def sample(self,
                num_samples:int,
-               current_device: int) -> Tensor:
+               current_device) -> Tensor:
         """
         Samples from the latent space and return the corresponding
         image space map.
@@ -176,3 +204,77 @@ class VanillaVAE(nn.Module):
 
         return self.forward(x)[0]
    
+class BetaSchedulerCyclic:
+    """linear cyclic beta scheduler
+    """
+
+    def __init__(self, start=0.0, stop=1.0,  period=1000, ratio=0.5):
+        # names are constant, cyclic, and monotonic
+        self.start = start
+        self.stop = stop
+        self.period = period
+        self.ratio = ratio
+        self.step = (stop-start)/(period*ratio)
+        self.v, self.i = start - self.step, 0
+    
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.i < self.period:
+            self.i += 1
+            if self.v < self.stop:
+                self.v += self.step
+        else:
+            self.v, self.i = self.start, 0
+        return self.v
+
+class BetaSchedulerMono:
+    """linear monotonically increasing scheduler
+    """
+
+    def __init__(self, start=0.0, stop=1.0,  period=1000):
+        # names are constant, cyclic, and monotonic
+        self.start = start
+        self.stop = stop
+        self.period = period
+        self.ratio = ratio
+        self.step = (stop-start)/(period*ratio)
+        self.v, self.i = start - self.step, -1
+    
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.i < self.period:
+            self.i += 1
+            if self.v < self.stop:
+                self.v += self.step
+        return self.v
+
+class IsoVAE(VanillaVAE):
+    def __init__(self,
+                 in_channels: int,
+                 latent_dim: int,
+                 res: int,
+                 layer_count: int,
+                 layer_mult: int,
+                 down: bool = True) -> None:
+        """[summary]
+
+        Parameters
+        ----------
+        same as VanillaVAE, see above
+
+        down : bool, optional
+            true if only isometry in first half of network, by default True
+        """
+        # super super init
+        super().__init__(in_channels,
+                 latent_dim,
+                 res,
+                 layer_count,
+                 layer_mult)
+        # TODO: Initialize to delta
+    
+    # TODO: change loss
